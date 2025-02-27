5dcd92b75bba8cea09c8fe29634b28499d83af9b
Emmanuel Moscoso <emoscoso21@alumnos.uaq.mx>
2025-02-26 19:25:48 -0600

    Database config

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
 
+
 @Module({
   controllers: [EmployeesController],
   providers: [EmployeesService],
