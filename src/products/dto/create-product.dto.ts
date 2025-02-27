1f076077c982078c3ec80035bf181e57fbb4ff69
Emmanuel Moscoso <emoscoso21@alumnos.uaq.mx>
2025-02-26 21:41:47 -0600

    Modules and ORM

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductDto {}
+import { IsInt, IsNumber, IsOptional, IsString, IsUUID, Max, MaxLength } from "class-validator";
+
+export class CreateProductDto {
+@IsUUID("4")
+@IsString()
+@IsOptional()
+productId: string;
+@IsString()
+@MaxLength(50)
+productName: string;
+@IsNumber()
+price: number;
+@IsInt()
+countSeal: number;
+@IsUUID("4")
+@IsString()
+@IsOptional()
+provider: string;
+}
