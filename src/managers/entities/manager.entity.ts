c6c68b68ef7130c766ceff19ae7d8e89e9c093d2
Emmanuel Moscoso <emoscoso21@alumnos.uaq.mx>
2025-03-08 22:53:47 -0600

    User relations to manager and employees

--- src/managers/entities/manager.entity.ts
+++ src/managers/entities/manager.entity.ts
@@ -1,5 +1,6 @@
+import { User } from "src/auth/entities/user.entity";
 import { Location } from "src/locations/entities/location.entity";
-import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Manager {
@@ -16,5 +17,11 @@ export class Manager {
 
     @OneToOne(()=>Location)
     location: Location
+
+    @OneToOne(() => User)
+    @JoinColumn({
+        name: "UserId"
+    })
+    user: User;
 }
 
