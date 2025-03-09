55f2741e2185cb37893901484cb44f5572a74035
Emmanuel Moscoso <emoscoso21@alumnos.uaq.mx>
2025-03-08 22:34:40 -0600

    feat: Auth decorator implemented

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -2,13 +2,12 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, U
 import { ProvidersService } from './providers.service';
 import { CreateProviderDto } from './dto/create-provider.dto';
 import { UpdateProviderDto } from './dto/update-provider.dto';
-import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { UserData } from 'src/auth/decorators/user.decorators';
 import { User } from 'src/auth/entities/user.entity';
-import { Roles } from 'src/auth/decorators/roles.decorator';
 import { RolesGuard } from 'src/auth/guards/roles.guards';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
-@UseGuards(AuthGuard)
+
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
@@ -17,9 +16,8 @@ export class ProvidersController {
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.providersService.create(createProviderDto);
   }
-
  
-  @Roles(["Admin"])
+  @Auth("Admin")
   @UseGuards(RolesGuard)
   @Get()
   findAll(@UserData() user: User) {
