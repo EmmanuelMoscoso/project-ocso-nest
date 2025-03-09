0ddbc4f9950248e2b80914707eaa12b4898346bd
Emmanuel Moscoso <emoscoso21@alumnos.uaq.mx>
2025-03-08 22:18:19 -0600

    feat:User decorator for routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,47 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { ProductsModule } from './products/products.module';
-import { ConfigModule } from '@nestjs/config';
 import { ProvidersModule } from './providers/providers.module';
 import { ManagersModule } from './managers/managers.module';
 import { LocationsModule } from './locations/locations.module';
 import { RegionsModule } from './regions/regions.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { EXPIRES_IN, JWT_KEY } from './auth/constants/jwt.constants';
+
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: process.env.host,
-    port: +(process.env.port || 5432),
-    username: 'postgres',
-    password: process.env.pass,
-    database: process.env.name,
-    entities: [],
-    autoLoadEntities: true,
-    synchronize: true,
-  }),
-    EmployeesModule, ProductsModule, ProvidersModule, ManagersModule, LocationsModule, RegionsModule, AuthModule],
+    ConfigModule.forRoot({
+      isGlobal: true,  // Makes env variables accessible globally
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    EmployeesModule,
+    ProductsModule,
+    ProvidersModule,
+    ManagersModule,
+    LocationsModule,
+    RegionsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/constants/jwt.constants.ts
@@ -0,0 +1,2 @@
+export const JWT_KEY = "BIHFNIBAVFUAUHKBHDAGBIUAÑ"
+export const EXPIRES_IN = '60s'
