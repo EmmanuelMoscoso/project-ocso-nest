d7588e35b490c6c2fe4d38ed4e5848fd10a8c406
Emmanuel Moscoso <emoscoso21@alumnos.uaq.mx>
2025-03-22 22:44:23 -0600

    fear: Enabled cors for front

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.allowedOrigin
+    }
+  });
   const config = new DocumentBuilder()
     .setTitle('Ocso API')
     .setDescription('API for ocso management')
     .setVersion('0.9')
+    .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
@@ -17,6 +22,6 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true,
   }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
